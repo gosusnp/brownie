d196f4219093290851fb1ecd2f7b42abf1385f81
Jimmy <gosu.snp@gmail.com>
2013-10-26 07:40:49 +0200

    Add a cardStore service.
    Handle saving/loading of cards through localStorage.

---
+++ js/services/card_store.js
@@ -0,0 +1,21 @@
+angular.module('brownie').factory('cardStore', [
+    function() {
+        var cardStore = {
+            cards: [],
+
+            getCards: function() {
+                data = localStorage.getItem("cards");
+                console.log(data);
+                if (data !== null) {
+                    cardStore.cards = angular.fromJson(data);
+                } else {
+                    cardStore.cards = [{items: []}];
+                }
+            },
+            saveCards: function() {
+                localStorage.setItem("cards", angular.toJson(cardStore.cards));
+            },
+        };
+        return cardStore;
+    }
+]);
