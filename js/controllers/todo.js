d654115784e300ebd3eb623c1826d7d083dbec82
Jimmy <gosu.snp@gmail.com>
2013-10-26 07:57:51 +0200

    Add button to add task to todo.

--- js/controllers/todo.js
+++ js/controllers/todo.js
@@ -7,16 +7,22 @@ angular.module('brownie').controller('TodoCtrl', [
     	      count += task.done ? 0 : 1;
     	    });
     	    return count;
-    	  };
+	};
     	  
-    	  $scope.progression = function() {
+	$scope.progression = function() {
       	    var count = 0;
       	    angular.forEach($scope.item.tasks, function(task) {
       	      count += task.done ? 0 : 1;
       	    });
       	    var progression = Math.floor((($scope.item.tasks.length - count) / ($scope.item.tasks.length)) * 100)
       	    return progression;
-      	  };
+	};
+
+	$scope.addTask = function() {
+		// FIXME default text should be '', however, if it is set to '',
+                // it is not editable through the UI
+		$scope.item.tasks.push({text: '...', done: false});
+	}
     }
 ]);
 
