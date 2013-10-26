1e30f1630adec8072609bef6234689bb5863bac1
Jimmy <gosu.snp@gmail.com>
2013-10-26 09:44:03 +0200

    Update canvas

--- js/controllers/canvas.js
+++ js/controllers/canvas.js
@@ -1,15 +1,4 @@
 angular.module('brownie').controller('CanvasCtrl', [
-    '$scope',
-    function($scope) {
-    	console.log("I am a canvas controller :)");
-    	$rootScope.$watch('windowWidth', function(newVal, oldVal) {
-    		resizeElement();
-    	});
+    function() {
     }
-]).run(['$rootScope', '$window', function($rootScopr,$window) {
-	$rootScope.windowWidth = $window.outerWidth;
-	angular.element($window).bind('resize',function(){
-		$rootScope.windowWidth = $window.innerWidth;
-		$rootScope.$apply('windowWidth');
-	});
-}]);
+]);
