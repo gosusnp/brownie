73a071720659224387dfb5a56e845a0a60ad9282
Jimmy <gosu.snp@gmail.com>
2013-10-27 06:24:02 +0100

    Fix canvas on touch devices.

--- js/directives/drawing.js
+++ js/directives/drawing.js
@@ -10,52 +10,43 @@ angular.module('brownie').directive("drawing", [
                 var ctx = element[0].getContext('2d');
                 // variable that decides if something should be drawn on mousemove
                 var drawing = false;
-                // the last coordinates before the current move
-                var lastX,
-                    lastY,
-                    currentX,
-                    currentY;
 
                 var getX = function (event) {
                     if(event.type.indexOf("touch") > -1) {
-                        return event.targetTouches[0].pageX;
+                        return event.targetTouches[0].pageX - angular.element(element).offset().left;
                     } else {
                         return event.offsetX;
                     }
                 };
                 var getY = function (event) {
                     if(event.type.indexOf("touch") > -1) {
-                        return event.targetTouches[0].pageY;
+                        return event.targetTouches[0].pageY - angular.element(element).offset().top;
                     } else {
                         return event.offsetY;
                     }
                 };
                 var moveStart = function(event) {
-                    event.preventDefault();
-                    lastX = getX(event);
-                    lastY = getY(event);
-                    // begins new line
-                    ctx.beginPath();
                     drawing = true;
+                    ctx.beginPath();
+                    ctx.moveTo(getX(event),getY(event));
+                    event.preventDefault();
                 };
                 var moveContinue = function(event) {
-                    event.preventDefault();
                     if (drawing) {
-                        // get current mouse position
-                        currentX = getX(event);
-                        currentY = getY(event);
-
-                        draw(lastX, lastY, currentX, currentY);
-
-                        // set current coordinates to last one
-                        lastX = currentX;
-                        lastY = currentY;
+                        ctx.lineTo(getX(event), getY(event));
+                        ctx.strokeStyle = "#4bf";
+                        ctx.stroke();
+                        modelAttribute.assign(scope, element[0].toDataURL());
                     }
+                    event.preventDefault();
                 };
                 var moveEnd = function(event) {
+                    if (drawing) {
+                        ctx.stroke();
+                        ctx.closePath();
+                        drawing = false;
+                    }
                     event.preventDefault();
-                    // stop drawing
-                    drawing = false;
                 };
 
                 element[0].addEventListener('touchstart', moveStart, false);
@@ -69,21 +60,6 @@ angular.module('brownie').directive("drawing", [
                 element.bind('mouseup', moveEnd);
 
                 // canvas reset
-                function reset(){
-                    element[0].width = element[0].width;
-                }
-                function draw(lX, lY, cX, cY){
-                    // line from
-                    ctx.moveTo(lX,lY);
-                    // to
-                    ctx.lineTo(cX,cY);
-                    // color
-                    ctx.strokeStyle = "#4bf";
-                    // draw it
-                    ctx.stroke();
-                    modelAttribute.assign(scope, element[0].toDataURL());
-                }
-
                 var loadImage = function(data) {
                     var img = new Image();
                     img.onload = function() {
